26ac11b2b05811c43ece53b02ff3331708ef4b4e
mh550 <mh550@duke.edu>
2025-02-08 00:05:17 -0500

    Containerized Video Proc; Enable cloud storage & updated API accordingly

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,45 @@
 import express from "express";
+import { convertVideo, deleteProcessedVideo, deleteRawVideo, downloadRawVideo, setupDirectories } from "./storage";
+
+setupDirectories();
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello world!");
+app.post("/process-video", async (req, res) => {
+    // get the bucket and fileName from cloud PubSub message
+    let data;
+    try {
+        const message = Buffer.from(req.body.message.data, 'base64').toString('utf-8');
+        data = JSON.parse(message);
+        if (!data.name) {
+            throw new Error('invalid message syntax');
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(400).send('Bad request: missing filename.');
+        return;
+    }
+
+    const inputFileName = data.name;
+    const outputFileName = `processed-${inputFileName}`;
+
+    await downloadRawVideo(inputFileName);
+
+    try {
+        convertVideo(inputFileName, outputFileName)
+    } catch(err) {
+        Promise.all([
+            deleteProcessedVideo(outputFileName),
+            deleteRawVideo(inputFileName)
+        ])
+        console.log(err);
+        res.status(500).send('Internal server error: video processing failed.');
+        return;
+    }
 });
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Video processing service listening at http://localhost:${port}`);
 })
